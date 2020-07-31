ca42b895d74d24c3bb0563b6bac3a2277f116af3
Mona Lisa <patrinoua@gmail.com>
2020-07-31 15:41:30 +0200

    update text

--- client/src/components/NextSteps.js
+++ client/src/components/NextSteps.js
@@ -8,35 +8,18 @@ function NextSteps() {
   return (
     <SectionContainer>
       <p>
-        PingPongBuddies is only the start! The broader vision is to create an
-        app where people can find other people for any activity they wish: From
-        ping pong to playing chess, going to the fleamarket or even co-working
-        in a cafe!
+        PingPongBuddies is only the start! The idea is to create an app where
+        people can find other people for any activity they wish: From ping pong
+        to playing chess, going to the fleamarket or even co-working in a cafe!
       </p>
-      <h2>Timeline</h2>
-      1. Finish the development of PingPongBuddies.
-      <br />
-      2. Develop the app further to offer a wide variety of activities:
-      <h1>Buddies</h1>
       <p>
-        So far, it is called "Buddies". A brand that reflects energy, freshness,
-        activeness, empowerment to all, trust and the feeling of an
-        international friendly community is on its way, including
+        This is called... "Buddies" so far! But should be replaced soon by a
+        name that reflects energy, freshness, activeness, empowerment to all,
+        trust and the feeling of an international friendly community, in
+        addition to Logo, Colors and UI.
       </p>
-      <ul>
-        <li>Brand Name</li>
-        <li>Logo</li>
-        <li>Colors</li>
-        <li>UI</li>
-      </ul>
-      <p>Therefore I am currently looking for creative partners!</p>
       <p>Sounds interesting/wanna get involved?</p>
-      <p>
-        Write me a message with wether you want to be a creative cofounder or
-        want to contribute as a freelancer. Don't forget to include your
-        Portfolio with samples of work and rates. Looking forward to hearing
-        from you!
-      </p>
+      <p>Feel free to get in touch!</p>
       <br />
       <Fab
         variant='extended'
