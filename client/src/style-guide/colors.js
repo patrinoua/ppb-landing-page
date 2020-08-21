ee9b2d29d6fa08c51ce656fb14fca98d295ec785
Mona Lisa <patrinoua@gmail.com>
2020-08-22 01:15:28 +0200

    add typography and colors

---
+++ client/src/style-guide/colors.js
@@ -0,0 +1,24 @@
+export const colors = {
+  cetaceanBlue: '#001134',
+  sapphire: '#002080',
+  imperialBlue: '#002699',
+  strongBlue: '#002DB3',
+  trueBlue: '#005CDB',
+  navyBlue: '#007FE8',
+  pacificBlue: '#009DDD',
+
+  black: '#000000',
+  charcoal: '#333333',
+  mediumGray: '#8C8C8C',
+  lightGray: '#CDCDCD',
+  whisper: '#E6E6E6',
+  pearl: '#F2F2F2',
+  snow: '#FBFBFB',
+  lightSnow: '#FCFCFC',
+
+  white: '#FFFFFF',
+  darkMagenta: '#B1009D',
+  iris: '#00B8C3',
+  carribeanGreen: '#00D0A3',
+  riceFlower: '#D6F0F3',
+}
---
+++ client/src/style-guide/typography.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components'
+import { colors } from './colors'
+
+export const H1 = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 34px;
+  color: ${colors.charcoal};
+  font-style: normal;
+  // letter-spacing: 0.05em;
+`
+export const H2 = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 26px;
+  color: ${colors.charcoal};
+`
+export const H3 = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  color: ${({ color }) => color || colors.charcoal};
+`
+export const Regular = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: ${colors.charcoal};
+`
+
+export const Prompt = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  color: ${colors.charcoal};
+  margin: 30px 0;
+`
+export const Subtitle = styled.div`
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 21px;
+  color: ${({ color }) => color || colors.mediumGray};
+`
