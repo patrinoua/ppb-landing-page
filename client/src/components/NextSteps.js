9db9acf6b5680c2d3dc298fc164c8db9db748fc0
Mona Lisa <patrinoua@gmail.com>
2020-08-22 02:03:50 +0200

    Refactor NextSteps

--- client/src/components/NextSteps.js
+++ client/src/components/NextSteps.js
@@ -1,32 +1,25 @@
 import React from 'react'
-import { SectionContainer, ScrollFullContainer, Description } from './elements'
-import Fab from '@material-ui/core/Fab'
+import { SectionContainer } from './elements'
+import Button from '@material-ui/core/Button'
 
 import { NavigationButtonItem } from './elements'
-
+import { Prompt } from '../style-guide/typography'
 function NextSteps() {
   return (
     <SectionContainer>
-      <p>
+      <Prompt>
         PingPongBuddies is only the start! The idea is to create an app where
         people can find other people for any activity they wish: From ping pong
         to playing chess, going to the fleamarket or even co-working in a cafe!
-      </p>
-      <p>
-        This is called... "Buddies" so far! But should be replaced soon by a
-        name that reflects energy, freshness, activeness, empowerment to all,
-        trust and the feeling of an international friendly community, in
-        addition to Logo, Colors and UI.
-      </p>
-      <p>Sounds interesting/wanna get involved?</p>
-      <p>Feel free to get in touch!</p>
-      <br />
-      <Fab
+      </Prompt>
+      <Prompt>Sounds interesting/wanna get involved?</Prompt>
+      <Prompt>Get in touch!</Prompt>
+      <Button
         variant='extended'
         style={{ width: '170px', marginLeft: '10px', marginTop: '20px' }}
       >
         <NavigationButtonItem to='/contact'>Contact</NavigationButtonItem>
-      </Fab>
+      </Button>
     </SectionContainer>
   )
 }
